3bceb09cdc9e91f8c80b5eaf5ee59429962187fc
rohollah alamalhoda <s.r.alamalhoda@gmail.com>
2021-09-19 23:50:25 +0430

    شروع محاسبه سود.

--- api/scripts/script.py
+++ api/scripts/script.py
@@ -1,10 +1,18 @@
+import datetime
+
 import api.models
 
-m = api.models.Ashkhas.objects.get(id=2)
+m = api.models.Ashkhas.objects.get(id=266)
 print(m)
-ta = '2021-07-26'
-seporde = m.tarakonesh_sum_ta(kind=1, ta=ta)
-print(seporde)
+az_date = datetime.date(year=2021, month=4, day=21)  # 1400/01/31
+ta_date = datetime.date(year=2021, month=5, day=21)  # 1400/02/31
+mo = m.mohasebe_sod_old(az_date=az_date, ta_date=ta_date)
+print(len(mo))
+summ = 0
+for m1 in mo:
+    summ = m1.calculated_amount + summ
+    print(m1)
 
-bardasht_sod = m.tarakonesh_sum_ta(kind=3, ta=ta)
-print(bardasht_sod)
+print(summ)
+
+
