6e4d4a2f555f4e4e93267263732bbe9ded87500d
ali musavi <s.alimusavi1359@hotmail.com>
2021-10-09 10:37:35 +0330

    add csrf_exempt to url

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 from graphene_file_upload.django import FileUploadGraphQLView
 
 from . import views
@@ -9,6 +10,6 @@ urlpatterns = [
     path("", views.home, name='home'),
     path("login/", views.login_user, name='login_user'),
     # path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True))),
-    url(r'^graphql', FileUploadGraphQLView.as_view(graphiql=True)),
+    url(r'^graphql', csrf_exempt(FileUploadGraphQLView.as_view(graphiql=True))),
     path("privategraphql", PrivateGraphQLView.as_view(graphiql=True)),
 ]
